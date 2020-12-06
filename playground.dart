854dbb929ef2ee987feb3a80141b833f41eaf000
Yudha Nawawi <yudhanawawi12@gmail.com>
2020-12-06 18:47:26 +0700

    feat: while and do while

--- playground.dart
+++ playground.dart
@@ -1,21 +1,53 @@
 main() {
   /**
-   * forEach()
-   * Foreach will loop the element of array
-   * then call the higher order function every loop
-   * and pass the element to higher order function's
-   * argument
+   * while()
+   * While will loop 
+   * as long as the condition
+   * is true
    * 
    * syntax
-   * arr.forEach((element) => {
-   *  //statement
-   * })
+   * while(condition) {
+   *   //statement
+   * }
    */
 
-  var a = [1, 2, 3, 4, 5];
+  int i = 0;
 
-  //print all element in array a
-  a.forEach((element) {
-    print(element);
-  });
+  print('While | $i to 9');
+  /**
+   * loop as long as i 
+   * is less than 10
+   */
+  while (i < 10) {
+    //print value of i
+    print(i);
+
+    //increment i
+    //ensure the loop
+    //not to be infite
+    i++;
+  }
+
+  /**
+   * do .. while();
+   * Do While will loop 
+   * as long as the condition
+   * is true
+   * 
+   * The difference betwen while
+   * and do while is the do while
+   * will execute the statement 1x
+   * before check the condition
+   */
+
+  print('\nDo While | $i to 0');
+  do {
+    //print the value of i
+    print(i);
+
+    //ensure loop will infinte
+    //by decrement the value of
+    //i in every loop
+    i--;
+  } while (i >= 0);
 }
