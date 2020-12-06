0989f495b4d267e9cfc4e22c70153185b5573b3f
Yudha Nawawi <yudhanawawi12@gmail.com>
2020-12-06 17:00:23 +0700

    feat: operators

--- playground.dart
+++ playground.dart
@@ -1,22 +1,81 @@
 main() {
-  //null means the value is blank
+  /**
+   * Arithmetic Operators
+   * + addition
+   * - substraction
+   * * multiplication
+   * / division
+   * % modulo
+   */
+
+  int a = 2;
+  int b = 2;
+
+  print('a: $a | b: $b');
+
+  int c = a + b;
+  print('$a + $b = $c');
+
+  c = a - b;
+  print('$a - $b = $c');
+
+  c = a * b;
+  print('$a * $b = $c');
+
+  c = (a / b).round();
+  print('$a / $b = $c');
+
+  c = a % b;
+  print('$a % $b = $c');
 
   /**
-   * for example define variable 
-   * without a value
-   * 
-   * the default value will null
+   * Relational Operators
+   * == equal
+   * != not equal
+   * > greater than
+   * < less than
+   * >= greater than or equal
+   * <= less than or equal
    */
-  int number;
 
-  print("\Implicit Null");
-  print(number); // print null to screen
+  print('$a equal to $b : ' + (a == b).toString()); //true
+  print('$a not equal to $b : ' + (a != b).toString()); //false
+  print('$a greater than $b : ' + (a > b).toString()); //false
+  print('$a less than $b : ' + (a < b).toString()); //false
+  print('$a greater than or equal to $b : ' + (a >= b).toString()); //true
+  print('$a less than or equal to $b : ' + (a <= b).toString()); //true
 
   /**
-   * We can also set variable to null explicitly
+   * Unary Operators
+   * ! not(flip true to false and vice versa)
+   * ++ increment
+   * -- decrement
+   * ? null check
+   * ?? return val when null
+   * ??= assign val when null
    */
-  int number2 = null;
 
-  print("\nExplicit Null");
-  print(number2); //also print null
+  a++; //increase value of a by 1
+  print('increment a : $a');
+  b--; //reduce value of a by 1
+  print('decrement b : $b');
+
+  Number number = Number();
+
+  print('number = Number Object');
+  print('number.val is ' + number.val.toString());
+
+  number = null;
+  print('number = null');
+  print('(? operator to check null and return null)\nnumber.val is ' +
+      number?.val.toString());
+  print('(?? operator to specify default val when null)\nnumber.val is ' +
+      (number?.val ?? 1).toString());
+  int valOfNumber;
+  valOfNumber ??= 1; //set value if valOfNumber is null
+  print('set valOfNumber from null to $valOfNumber');
+}
+
+class Number {
+  int val = 29;
 }
