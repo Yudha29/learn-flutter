20d6b3cc8617eda2fa28b8963e002cc698d468e2
Yudha Nawawi <yudhanawawi12@gmail.com>
2020-12-06 14:59:12 +0700

    feat: normal & raw string

--- playground.dart
+++ playground.dart
@@ -1,55 +1,28 @@
+//String
+
 main() {
   /**
-   * int - integer type
-   * double - floating point type
-   * String - characters type
-   * bool - boolean type
-   * dynamic - we can change the variable type at run time
+   * String can be construct with single/double quote
    */
 
-  /**
-   * Integer type
-   */
-  int number = 100;
-  var number2 = 100; //use var to make type inference to int
+  //single quote
+  String text = 'You are good person';
+  String text2 = 'it\'s a dog'; //use \ (backslash) to escape ' character
 
-  print('Integer type');
-  print('number: $number\n number with var: $number2\n');
+  //double quote
+  String text3 = "You are good person";
+  String text4 = "it's a dog";
 
-  /**
-   * Double type
-   */
-  double dnumber = 100.11;
-  var dnumber2 = 100.11; //use var to make type inference to double
+  print('String with single quote');
+  print(text);
+  print(text2);
+  print("\nString with double quote");
+  print(text3);
+  print(text4);
 
-  print('Double type');
-  print('number: $dnumber\n number with var: $dnumber2\n');
+  //with raw string, special character will not treated
+  String rawString = r'Hello \n Yes'; //the \n will be printed to the screen
 
-  /**
-   * String type
-   */
-  String name = "Yudha";
-  var name2 = "Nawawi"; //use var to make type inference to String
-
-  print('String type');
-  print('name: $name\n name with var: $name2\n');
-
-  /**
-   * Double type
-   */
-  bool boolean = false;
-  var boolean2 = true; //use var to make type inference to Boolean
-
-  print('Boolean type');
-  print('boolean: $boolean\n boolean with var: $boolean2\n');
-
-  /**
-   * Dynamic type
-   */
-  dynamic dynamicVar = "Yudha";
-  print('Dynamic type');
-  print('Now dynamicVar is string with value $dynamicVar');
-
-  dynamicVar = 29;
-  print('Now dynamicVar is integer with value $dynamicVar\n');
+  print('\nRaw String');
+  print(rawString);
 }
