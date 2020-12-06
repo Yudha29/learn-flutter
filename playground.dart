e90cb5b7ecc64a1154ad8d6109578bac16ddedf7
Yudha Nawawi <yudhanawawi12@gmail.com>
2020-12-06 12:10:22 +0700

    feat: describe var keyword

---
+++ playground.dart
@@ -0,0 +1,18 @@
+/**
+ * import for dart:core will be imported automatically
+ * so i dont have to write it in my code
+ */
+//import 'dart:core';
+
+main() {
+  /**
+   * use var will use type inference for the value
+   * for this example the type of firstname will be string
+   * because i put string value to it
+   */
+  var firstName = 'Yudha';
+  String lastName = 'Nawawi';
+
+  //use + to concat string
+  print(firstName + ' ' + lastName);
+}
