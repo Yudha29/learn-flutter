ab8bbfa2a4b3840616d4cea024a70c6cf085874b
Yudha Nawawi <yudhanawawi12@gmail.com>
2020-12-06 15:26:46 +0700

    feat: constant

--- playground.dart
+++ playground.dart
@@ -1,42 +1,22 @@
-//String
-
 main() {
-  /**
-   * convert
-   * string '1' to integer 1
-   */
-  var one = int.parse('1');
-  /**
-   * verify if the value of one
-   * is equal to 1
-   * 
-   * assert() will throw error if 
-   * one is not equal to 1
-   */
-  assert(one == 1);
+  const int number = 1; //define int const
+  const bool booelan = true; //define bool const
+  const String text = "String constant"; //define String const
 
   /**
-   * Convert
-   * String '1.1' to double 1.1
+   * Print the value and 
+   * the type of constant
+   * at run time
    */
-  var onePointOne = double.parse('1.1');
-  assert(onePointOne == 1.1);
+  print(number);
+  //property runtimeType used to get the type of variable at runtime
+  print(number.runtimeType);
 
-  /**
-   * Convert 
-   * Double 1.1 to String '1.1'
-   */
-  var doubleStr = 1.1.toString();
-  assert(doubleStr == '1.1');
+  print('\n');
+  print(booelan);
+  print(booelan.runtimeType);
 
-  var doubleStrFix =
-      1.1111111.toStringAsFixed(2); //specify digit behind the coma
-  assert(doubleStrFix == '1.11');
-
-  /**
-   * Convert 
-   * Integer 1 to String '1'
-   */
-  var intStr = 1.toString();
-  assert(intStr == '1');
+  print('\n');
+  print(text);
+  print(text.runtimeType);
 }
